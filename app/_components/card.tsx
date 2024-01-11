e71b510f74e3cde2892f3f66fd0017e415656cb4
souravdasdip <souravdasdip65@gmail.com>
2024-01-11 13:03:53 +0600

    layout transition

--- app/_components/card.tsx
+++ app/_components/card.tsx
@@ -4,6 +4,7 @@ import { useFeatureStore } from '@/store/store';
 import classNames from 'classnames';
 import Image from 'next/image';
 import React from 'react';
+import { motion } from 'framer-motion'
 
 type FeatureCardProps = {
   id: string;
@@ -20,8 +21,12 @@ export const FeatureCard = ({  id, gradient, children, img }: FeatureCardProps) 
   const inVIewFeature = useFeatureStore((state) => state.inViewFeature);
   const setFullScreenFeature = useFeatureStore(state => state.setfullScreenFeature)
   const fullScreenFeature = useFeatureStore(state => state.fullScreenFeature)
+
+  console.log(inVIewFeature, fullScreenFeature);
+  
   return (
     <div
+    
       className={classNames(
         'absolute inset-0 h-full w-full rounded-2xl p-4 transition-opacity',
         inVIewFeature === id ? 'active-card opacity-100' : 'opacity-0 pointer-events-none'
@@ -30,9 +35,15 @@ export const FeatureCard = ({  id, gradient, children, img }: FeatureCardProps) 
 
       <div className={classNames('gradient rounded-2xl origin-bottom-left absolute inset-0 bg-gradient-to-br', gradient)}>
 
-      <button onClick={() => setFullScreenFeature(id)} className='show-me-btn absolute bottom-4 right-4 bg-black mt-2 px-4 py-1 shadow-lg rounded-full text-white'>Show</button>
+      <button onClick={() => setFullScreenFeature(id)} className='show-me-btn absolute bottom-2 right-4 bg-black mt-2 px-4 py-1 shadow-lg rounded-full text-white'>Show</button>
       </div>
-      <Image className={classNames(fullScreenFeature === id ? "scale-0" : "scale-100",'aspect-square w-full h-[90%] rounded-xl shadow-lg transition-transform')} width={500} height={500} src={img} alt={id}/>
+        {
+          !fullScreenFeature && (
+          <motion.div layoutId={"visual-img"} className='h-[85%]'>
+            <Image className={classNames(inVIewFeature === id ? "scale-100" : "scale-0",'aspect-square w-full h-[100%] rounded-xl shadow-lg transition-transform')} width={500} height={500} src={img} alt={id}/>
+          </motion.div>
+          )
+        }
         
     </div>
   );
--- app/_components/visual.tsx
+++ app/_components/visual.tsx
@@ -1,6 +1,7 @@
 import { useFeatureStore } from "@/store/store"
 import classNames from "classnames"
 import Image from "next/image"
+import { motion } from 'framer-motion'
 
 const Visual = ({children, id}: {children: React.ReactNode, id: string}) => {
     const fullScreenFeature = useFeatureStore(state => state.fullScreenFeature)
@@ -9,17 +10,22 @@ const Visual = ({children, id}: {children: React.ReactNode, id: string}) => {
         <div className={classNames("z-10 pointer-events-none opacity-0 fixed inset-0 flex items-center justify-center",
         `visual-${id}`)}>
             <div className="max-w-6xl px-4">
-            {children}
-
+                {children}
             </div>
         </div>
     )
 }
 
 export const MusicVisual = ({id}: {id: string}) => {
+    const fullScreenFeature = useFeatureStore(state => state.fullScreenFeature)
+
     return(
         <Visual id={id}>
-            <Image width={1200} height={1000} src={"https://images.unsplash.com/photo-1704635021570-fd5af2fadf77?q=80&w=1542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" />
+            {fullScreenFeature && (
+                <motion.div layoutId={"visual-img"}>
+                    <Image width={1200} height={1000} src={"https://images.unsplash.com/photo-1704635021570-fd5af2fadf77?q=80&w=1542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} alt="img" />
+                </motion.div>
+            )}
         </Visual>
     )
 }
